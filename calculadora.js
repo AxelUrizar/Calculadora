4c9a2ca4d59f2d3a5cea724dff06d6e69f12ac11
AxelUrizar <axelurizarmartin@gmail.com>
2022-01-21 10:53:16 +0100

    ejercicio calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+export const calculadora = {
+    suma: (a, b) => {
+        return a + b
+    },
+    
+    resta: (a, b) => {
+        return a - b
+    },
+    
+    multi: (a, b) => {
+        return a * b
+    },
+    
+    divi: (a, b) => {
+        return a / b
+    }
+}
+
+export const otrasCosas = null;
+
+const hola = 'hola'
+export default hola
---
+++ index.js
@@ -0,0 +1,7 @@
+import defaultExport, { calculadora } from "./calculadora.js";
+
+console.log(defaultExport);
+
+let resultadoSuma = calculadora.suma(1, 5);
+
+console.log(resultadoSuma);
